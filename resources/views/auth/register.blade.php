eea0e15eab9002078ae6d6d09b5641d8e4303c56
JehanKandy <jehankandy@gmail.com>
2023-06-25 10:16:57 +0530

    Update

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -119,17 +119,31 @@
                             </div>
                             <div class="form-group">
                                 <label for="password">{{ __('Password') }}</label>
-                                <input type="password" name="" id="" class="form-control">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                             <div class="form-group">
-                                <label for="cpassword">Confirm Password</label>
-                                <input type="password" name="" id="" class="form-control">
+                                <label for="cpassword">{{ __('Confirm Password') }}</label>
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                             <div class="form-group">
-                                <input type="submit" value="Register" class="login-btn">
+                                <button type="submit" class="login-btn">
+                                    {{ __('Register') }}
+                                </button>
                             </div>
                         </form>
-                        Already Have an Account ? <a href="">Login</a>
+                        Already Have an Account ? <a href="{{ route('login') }}">Login</a>
                         <hr>
                         <p class="develop">&copy;JehanKandy</p>
                     </div>
