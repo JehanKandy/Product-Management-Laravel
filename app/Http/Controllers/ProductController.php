18e0b53197691b9aae0b153fb05f1454b1306c86
JehanKandy <jehankandy@gmail.com>
2023-06-26 16:04:15 +0530

    Update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,7 +14,7 @@ class ProductController extends Controller
     public function index()
     {
         $product = Product::all();
-        return view('prodcut.index')->with('product', $product);
+        return view('product.index')->with('product', $product);
     }
 
     /**
