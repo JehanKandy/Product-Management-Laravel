c54417c76d7bfa3b34383af4bf383d43d7c831ed
JehanKandy <jehankandy@gmail.com>
2023-06-26 17:37:43 +0530

    Update

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -5,5 +5,13 @@
     <div class="admin-card">
         <h3>Name : <b> {{ $product->product_name }} </b></h3><br><br>
         <h3>description : <b> {{ $product->product_description }} </b></h3><br><br>
+        <div class="row">
+            <div class="col-lg-3">
+                Product Name
+            </div>
+            <div class="col-lg-9">
+                {{ $product->product_name  }}
+            </div>
+        </div>
     </div>
 @endsection
