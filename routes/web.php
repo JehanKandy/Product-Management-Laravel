4450c19f3a949b91fdee1269dcb12372cd4397cd
JehanKandy <jehankandy@gmail.com>
2023-06-26 07:31:42 +0530

    Update

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\UserController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,11 +16,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes([
+    'verify' => true
+]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('admin', function(){
+    return view('admin');
+})->name('admin')->middleware('admin');
+
+Route::get('staff', function(){
+    return view('staff');
+})->name('staff')->middleware('staff');
+
+Route::get('client', function(){
+    return view('client');
+})->name('client')->middleware('client');
+
+Route::resource("/users", UserController::class);
