8fe3c47ca1e30261b014944038e5b92d518fc66c
JehanKandy <jehankandy@gmail.com>
2023-06-26 15:42:53 +0530

    Update

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\MemberController;
 
 /*
 |--------------------------------------------------------------------------
