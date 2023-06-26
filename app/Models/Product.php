da5267b0d534f46cde18140deabe5f6660baa1ab
JehanKandy <jehankandy@gmail.com>
2023-06-26 15:57:06 +0530

    Update

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,5 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    protected $table = 'products';
+    protected $primaryKey = 'id';
+    protected $fillable = ['product_name', 'product_description', 'qty', 'product_price', 'product_status', 'created_at'];
     use HasFactory;
 }
