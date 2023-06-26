f0b0fd6648efcb8d5c7a21b9d579c3460cf76eaa
JehanKandy <jehankandy@gmail.com>
2023-06-26 16:35:08 +0530

    Update

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -14,8 +14,12 @@
                 <input type="text" name="product_name" id="" class="form-control"><br>
             </div>
             <div class="form-group">
-                <label for="product name">Product Description : </label>
-                <textarea name="" id="" cols="30" rows="10" class="form-control"></textarea>
+                <label for="product desc">Product Description : </label>
+                <textarea name="product_desc" id="" cols="20" rows="10" class="form-control" style="resize: none;"></textarea><br>
+            </div>
+            <div class="form-group">
+                <label for="product qty">Product Qty : </label>
+                <input type="number" name="product_qty" id="" class="form-control"><br>
             </div>
     
         </form>
