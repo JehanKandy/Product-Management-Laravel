27abb127e4cb293990071ecfdbb8ab18374204a0
JehanKandy <jehankandy@gmail.com>
2023-06-26 17:25:20 +0530

    Update

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -7,13 +7,19 @@
         <hr>
 
         @if ($errors->any())
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <strong>Error</strong> Following Input Errors are Found
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
             <div class="alert alert-danger">
                 <strong>Whoops!</strong> There were some problems with your input.<br><br>
-                <ul>
-                    @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
+
             </div>
         @endif
 
