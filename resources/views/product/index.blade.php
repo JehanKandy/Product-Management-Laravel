8c407bd6be7e5d90e85c6cebda7437ced1500e35
JehanKandy <jehankandy@gmail.com>
2023-06-26 16:17:40 +0530

    Update

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -18,6 +18,27 @@
                     <th>Product Add Date</th>
                 </tr>
             </thead>
+            <tbody>
+                @foreach($students as $item)
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $item->name }}</td>
+                        <td>{{ $item->address }}</td>
+                        <td>{{ $item->mobile }}</td>
+
+                        <td>
+                            <a href="{{ url('/student/' . $item->id) }}" title="View Student"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> View</button></a>
+                            <a href="{{ url('/student/' . $item->id . '/edit') }}" title="Edit Student"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
+
+                            <form method="POST" action="{{ url('/student' . '/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
+                                {{ method_field('DELETE') }}
+                                {{ csrf_field() }}
+                                <button type="submit" class="btn btn-danger btn-sm" title="Delete Student" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
         </table>
     </div>
 
