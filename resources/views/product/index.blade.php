d0e78f805145d532165a34f38679c9ee8ebe9c30
JehanKandy <jehankandy@gmail.com>
2023-06-26 16:14:20 +0530

    Update

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -3,5 +3,17 @@
 
 @section('content')
 
+    <div class="admin-card">
+        <h3>Products</h3>
+
+        <table class="table">
+            <thead class="table-primary">
+                <tr>
+                    <th>ID</th>
+                </tr>
+            </thead>
+        </table>
+    </div>
+
 @endsection
 
