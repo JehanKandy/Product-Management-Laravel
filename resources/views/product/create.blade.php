09a21af7b38497643fef039dd26b88fcc55d1c23
JehanKandy <jehankandy@gmail.com>
2023-06-26 16:39:05 +0530

    Update

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -11,19 +11,19 @@
         
             <div class="form-group">
                 <label for="product name">Product Name : </label>
-                <input type="text" name="product_name" id="" class="form-control"><br>
+                <input type="text" name="product_name" id="" class="form-control" placeholder="Product Name"><br>
             </div>
             <div class="form-group">
                 <label for="product desc">Product Description : </label>
-                <textarea name="product_desc" id="" cols="20" rows="10" class="form-control" style="resize: none;"></textarea><br>
+                <textarea name="product_desc" id="" cols="20" rows="10" class="form-control" style="resize: none;" placeholder="Product Description"></textarea><br>
             </div>
             <div class="form-group">
                 <label for="product qty">Product Qty : </label>
-                <input type="number" name="product_qty" id="" class="form-control"><br>
+                <input type="number" name="product_qty" id="" class="form-control" placeholder="Product Qty"><br>
             </div>
             <div class="form-group">
                 <label for="product Price">Product Price : </label>
-                <input type="number" name="product_price" class="form-control" min="0.00" max="10000.00" step="0.01" /><br>
+                <input type="number" name="product_price" class="form-control" min="0.00" max="10000.00" step="0.01" placeholder="Product Price"><br>
             </div>
     
         </form>
