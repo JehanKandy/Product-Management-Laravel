63cdc19980bc3e0959d18ae7ef668c1206d95f65
JehanKandy <jehankandy@gmail.com>
2023-06-26 16:32:28 +0530

    Update

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -4,12 +4,15 @@
 @section('content')
     <div class="admin-card">
         <h4><i class="fas fa-cart-plus"></i> Add New Product</h4>
-        <br>
         <hr>
 
         <form action="{{ url('student') }}" method="POST">
         @csrf
         
+            <div class="form-group">
+                <label for="product name">Product Name : </label>
+                <input type="text" name="product_name" id="" class="form-control">
+            </div>
     
         </form>
     </div>
