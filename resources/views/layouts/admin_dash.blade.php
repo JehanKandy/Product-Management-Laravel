21db70ee3163a03aa0b564515c0181b6aa0e4841
JehanKandy <jehankandy@gmail.com>
2023-06-26 15:36:05 +0530

    Update

--- resources/views/layouts/admin_dash.blade.php
+++ resources/views/layouts/admin_dash.blade.php
@@ -101,7 +101,7 @@
             <a href="{{url('/users')}}" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i
                     class="fas fa-user-tie me-2"></i>All Users</a>
             <a href="{{url('/users')}}" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i
-                    class="fas fa-user-tie me-2"></i>All Products</a>                    
+                    class="fas fa-gift me-2"></i>All Products</a>                    
 
             
         </div>
